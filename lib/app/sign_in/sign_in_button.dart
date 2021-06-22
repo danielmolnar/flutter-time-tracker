5c264d3ea074ff04a02af5a4ee97ca4acd269a43
Daniel Molnar <daniel-molnar@web.de>
2021-06-22 17:38:15 +0200

    created login page with buttons

---
+++ lib/app/sign_in/sign_in_button.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_time_tracker/common_widgets/custom_elevated_button.dart';
+
+class SignInButton extends CustomElevatedButton {
+  SignInButton({
+    @required String text,
+    Color color,
+    Color textColor,
+    VoidCallback onPressed,
+  })  : assert(text != null),
+        super(
+          child: Text(
+            text,
+            style: TextStyle(color: textColor, fontSize: 15.0),
+          ),
+          color: color,
+          onPressed: onPressed,
+          height: 50,
+        );
+}
---
+++ lib/app/sign_in/social_sign_in_button.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_time_tracker/common_widgets/custom_elevated_button.dart';
+
+class SocialSignInButton extends CustomElevatedButton {
+  SocialSignInButton({
+    @required String text,
+    @required String assetName,
+    Color color,
+    Color textColor,
+    VoidCallback onPressed,
+  })  : assert(text != null),
+        assert(assetName != null),
+        super(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Image.asset(assetName),
+              Text(
+                text,
+                style: TextStyle(color: textColor, fontSize: 15.0),
+              ),
+              Opacity(opacity: 0.0, child: Image.asset(assetName)),
+            ],
+          ),
+          color: color,
+          onPressed: onPressed,
+          height: 50,
+        );
+}
