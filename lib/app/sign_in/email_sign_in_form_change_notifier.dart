e117cde4fe493da0f405bdccb9aff554ae361ed6
Daniel Molnar <daniel-molnar@web.de>
2021-06-27 16:58:27 +0200

    implemented value notifier and change notifier provider for state management

---
+++ lib/app/sign_in/email_sign_in_form_change_notifier.dart
@@ -0,0 +1,133 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'email_sign_in_change_model.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_time_tracker/services/auth.dart';
+import 'package:flutter_time_tracker/common_widgets/form_submit_button.dart';
+import 'package:flutter_time_tracker/common_widgets/show_exception_alert_dialog.dart';
+
+class EmailSignInFormChangeNotifier extends StatefulWidget {
+  EmailSignInFormChangeNotifier({@required this.model});
+  final EmailSignInChangeModel model;
+
+  static Widget create(BuildContext context) {
+    final auth = Provider.of<AuthBase>(context, listen: false);
+    return ChangeNotifierProvider<EmailSignInChangeModel>(
+      create: (_) => EmailSignInChangeModel(auth: auth),
+      child: Consumer<EmailSignInChangeModel>(
+        builder: (_, model, __) => EmailSignInFormChangeNotifier(model: model),
+      ),
+    );
+  }
+
+  @override
+  _EmailSignInFormChangeNotifierState createState() =>
+      _EmailSignInFormChangeNotifierState();
+}
+
+class _EmailSignInFormChangeNotifierState
+    extends State<EmailSignInFormChangeNotifier> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final FocusNode _emailFocusNode = FocusNode();
+  final FocusNode _passwordFocusNode = FocusNode();
+
+  EmailSignInChangeModel get model => widget.model;
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    _emailFocusNode..dispose();
+    _passwordFocusNode.dispose();
+    super.dispose();
+  }
+
+  Future<void> _submit() async {
+    try {
+      await model.submit();
+      Navigator.of(context).pop();
+    } on FirebaseAuthException catch (e) {
+      showExceptionAlertDialog(
+        context,
+        title: 'Sign in failed',
+        exception: e,
+      );
+    }
+  }
+
+  void _emailEditingComplete() {
+    final newFocus = model.emailValidator.isValid(model.email)
+        ? _passwordFocusNode
+        : _emailFocusNode;
+    FocusScope.of(context).requestFocus(newFocus);
+  }
+
+  void _toggleFormType() {
+    model.toggleFormType();
+    _emailController.clear();
+    _passwordController.clear();
+  }
+
+  List<Widget> _buildChildren() {
+    return [
+      _buildEmailTextField(),
+      SizedBox(height: 8.0),
+      _buildPasswordTextField(),
+      SizedBox(height: 16.0),
+      FormSubmitButton(
+        text: model.primaryButtonText,
+        onPressed: model.canSubmit ? _submit : null,
+      ),
+      SizedBox(height: 8.0),
+      TextButton(
+        child: Text(model.secondaryButtonText),
+        onPressed: !model.isLoading ? _toggleFormType : null,
+      ),
+    ];
+  }
+
+  TextField _buildPasswordTextField() {
+    return TextField(
+        decoration: InputDecoration(
+          enabled: model.isLoading == false,
+          labelText: 'Password',
+          errorText: model.passwordErrorText,
+        ),
+        obscureText: true,
+        controller: _passwordController,
+        focusNode: _passwordFocusNode,
+        textInputAction: TextInputAction.done,
+        onEditingComplete: _submit,
+        onChanged: model.updatePassword);
+  }
+
+  TextField _buildEmailTextField() {
+    return TextField(
+        decoration: InputDecoration(
+          enabled: model.isLoading == false,
+          labelText: 'Email',
+          hintText: 'test@test.com',
+          errorText: model.emailErrorText,
+        ),
+        autocorrect: false,
+        keyboardType: TextInputType.emailAddress,
+        controller: _emailController,
+        focusNode: _emailFocusNode,
+        textInputAction: TextInputAction.next,
+        onEditingComplete: () => _emailEditingComplete(),
+        onChanged: model.updateEmail);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisSize: MainAxisSize.min,
+        children: _buildChildren(),
+      ),
+    );
+  }
+}
