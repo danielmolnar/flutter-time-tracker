8466f8f201b54437af40191d60fc9608545734ba
Daniel Molnar <daniel-molnar@web.de>
2021-06-28 17:05:17 +0200

    added firestore database

---
+++ lib/app/home/jobs_page.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_time_tracker/services/auth.dart';
+import 'package:flutter_time_tracker/services/database.dart';
+import 'package:flutter_time_tracker/app/home/models/job.dart';
+import 'package:flutter_time_tracker/common_widgets/show_alert_dialog.dart';
+import 'package:flutter_time_tracker/common_widgets/show_exception_alert_dialog.dart';
+
+class JobsPage extends StatelessWidget {
+  Future<void> _signOut(BuildContext context) async {
+    try {
+      final auth = Provider.of<AuthBase>(context, listen: false);
+      await auth.signOut();
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+  Future<void> _confirmSignOut(BuildContext context) async {
+    final didRequestSignOut = await showAlertDialog(
+      context,
+      title: 'Logout',
+      content: 'Are you sure that you want to logout?',
+      cancelActionText: 'Cancel',
+      defaultActionText: 'Logout',
+    );
+    if (didRequestSignOut == true) {
+      _signOut(context);
+    }
+  }
+
+  Future<void> _createJob(BuildContext context) async {
+    try {
+      final database = Provider.of<Database>(context, listen: false);
+      await database.createJob(Job(name: 'Blogging', ratePerHour: 10));
+    } on FirebaseException catch (e) {
+      showExceptionAlertDialog(
+        context,
+        title: 'Operation failed',
+        exception: e,
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Jobs'),
+        actions: <Widget>[
+          TextButton(
+            child: Text(
+              'Logout',
+              style: TextStyle(color: Colors.white),
+            ),
+            style: TextButton.styleFrom(
+              textStyle: const TextStyle(fontSize: 15),
+            ),
+            onPressed: () => _confirmSignOut(context),
+          )
+        ],
+      ),
+      body: _buildContents(context),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => _createJob(context),
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+
+  Widget _buildContents(BuildContext context) {
+    final database = Provider.of<Database>(context, listen: false);
+    return StreamBuilder<List<Job>>(
+      stream: database.jobsStream(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          final jobs = snapshot.data;
+          final children = jobs.map((job) => Text(job.name)).toList();
+          return ListView(children: children);
+        }
+        if (snapshot.hasError) {
+          return Center(child: Text('some error occured'));
+        }
+        return Center(child: CircularProgressIndicator());
+      },
+    );
+  }
+}
--- lib/app/landing_page.dart
+++ lib/app/landing_page.dart
@@ -1,8 +1,9 @@
-import 'home_page.dart';
+import 'home/jobs_page.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_time_tracker/services/auth.dart';
+import 'package:flutter_time_tracker/services/database.dart';
 import 'package:flutter_time_tracker/app/sign_in/sign_in_page.dart';
 
 class LandingPage extends StatelessWidget {
@@ -17,7 +18,9 @@ class LandingPage extends StatelessWidget {
           if (user == null) {
             return SignInPage.create(context);
           } else
-            return HomePage();
+            return Provider<Database>(
+                create: (_) => FirestoreDatabase(uid: user.uid),
+                child: JobsPage());
         }
         return Scaffold(
             body: Center(
